067145d45258f63d6c400d667dfb7c85027df482
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-07-29 09:47:47 +0200

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-10

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-10.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        counts=Counter(s)
+        maxOdd=0
+        minEven=float('inf')
+
+        for freq in counts.values():
+            if freq%2!=0:
+                maxOdd=max(maxOdd, freq)
+            else:
+                minEven=min(minEven, freq)
+        return maxOdd-minEven
