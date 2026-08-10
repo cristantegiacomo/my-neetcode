3c86442d25eb723f9566ce04fa9c631425c8b754
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-08-10 11:47:52 +0200

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        candidates.sort()
+        res = []
+        sub = [] 
+
+        def dfs(i, total):
+
+            if total == target:
+                res.append(sub.copy())
+                return
+
+            if i >= len(candidates) or total > target:
+                return
+
+            sub.append(candidates[i])
+            dfs(i+1, total + candidates[i])
+            sub.pop()
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+            dfs(i+1, total)
+               
+        dfs(0,0)
+        return res        
+
+# [1, 1, 1, 1, 2, 2, 5]
+# quando tu poppi è perche la somma è >= target quindi se hai poppato il terzo uno sicuramente non vuoi il quarto
