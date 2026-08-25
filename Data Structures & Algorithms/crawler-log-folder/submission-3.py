fec0e94576130da517023bd8e13ae8c557c0fc25
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-08-25 21:14:13 +0200

    Add: crawler-log-folder - submission-3

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        res = 0
+
+        for log in logs:
+            if log == "../":
+                if res:
+                    res -= 1
+            elif log != "./":
+                res += 1
+        return res            
