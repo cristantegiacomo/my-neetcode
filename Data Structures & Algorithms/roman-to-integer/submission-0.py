4676d3da8d1b6af20edc705e54422ab59230e7fe
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-09-05 17:11:01 +0200

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        num = 0
+
+        mp = {  "I": 1,
+                "V": 5,
+                "X": 10,
+                "L": 50,
+                "C": 100,
+                "D": 500,
+                "M": 1000 }
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and mp[s[i]] < mp[s[i+1]]:
+                num -= mp[s[i]]
+            else:
+                num += mp[s[i]]
+
+        return num
